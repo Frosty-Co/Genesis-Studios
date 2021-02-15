688fd141c5f752bc390d65c1fcaafa77dd61370e
iHavoc101 <69701784+iHavoc101@users.noreply.github.com>
2021-02-15 16:51:44 -0500

    Create Services.lua

---
+++ Modules/Services.lua
@@ -0,0 +1,10 @@
+local Services = getgenv().Services or setmetatable({}, {__index = function(Self, Index)
+    local NewService = game:GetService(Index)
+    if NewService then
+        Self[Index] = NewService
+    end
+    return NewService
+end})
+
+getgenv().Services = Services
+return Services
