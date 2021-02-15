23cc0dde8a72d1321db74af126f3d4005f36cc1c
iHavoc101 <69701784+iHavoc101@users.noreply.github.com>
2021-02-15 16:55:34 -0500

    Update Services.lua

--- Modules/Services.lua
+++ Modules/Services.lua
@@ -1,5 +1,5 @@
 local Services = getgenv().Services or setmetatable({}, {__index = function(Self, Index)
-    local NewService = game:GetService(Index)
+    local NewService = game:GetService(Index) or game:FindFirstChild(Index)
     if NewService then
         Self[Index] = NewService
     end
